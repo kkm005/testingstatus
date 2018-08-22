531edff001794085c4d9a7df78f8945253dd29af
kkm005 <33860679+kkm005@users.noreply.github.com>
2018-08-22 10:06:37 +0700

    Add files via upload

---
+++ testdb.php
@@ -0,0 +1,11 @@
+<?php
+require('./db/connect-db.php'); //คำสั่งเรียกใช้ไฟล์
+$sql_office = "SELECT * FROM tbl_office";
+$query_office = mysqli_query($conn,$sql_office);
+while ($obj = mysqli_fetch_array(query_office))
+{
+	echo $obj["office_name"]."<br>"; //จุด คือการเชื่อมข้อความ , "<br>" คือการขึ้นบรรทัดใหม่
+	
+}
+
+?>
