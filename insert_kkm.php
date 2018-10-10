4bbbb20545d68dcdf4402eba99d56eb28f830387
kkm005 <33860679+kkm005@users.noreply.github.com>
2018-10-10 10:05:24 +0700

    Add files via upload

--- insert_kkm.php
+++ insert_kkm.php
@@ -27,8 +27,18 @@
 		<div class="container">
 			<div class="row">
 				<div class="col-lg-2" style="background-color:yellow;">
-					<h4>col1</h4>
+					<div class="row">
+						<label for="usr">ชื่อ</label>
+						<input type="text" class="form-control" name="name" placement="ใส่ชื่อ">
 					</div>
+					<div class="row">
+						<label for="usr">นามสกุล:</label>
+						<input type="text" class="form-control" name="lastname" placement="ใส่นามสกุล">
+					</div>
+					<div class="row">
+						<label for="usr">email:</label>
+						<input type="text" class="form-control" name="email" placement="ใส่อีเมล">
+					</div>					
 				<div class="col-lg-10" style="background-color:blue;">
 					<h4>col2</h4>
 				</div>
